c9290b6ae6536b0fcfc5b3a2a56d2d391c6d28ab
Hobbyer <uskimeunyeob@gmail.com>
2025-03-13 17:30:00 +0900

    Secured, prePostAuthorize 활성화 및 효과(영향)

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -64,9 +67,17 @@ public class IndexController {
     return "redirect:/loginForm";
   }
 
+  @Secured("ROLE_ADMIN")
   @GetMapping("/info")
   public @ResponseBody String info() {
     return "개인정보";
+  }
+
+  // @PostAuthorize("hasRole('ROLE_ADMIN')")
+  @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+  @GetMapping("/data")
+  public @ResponseBody String data() {
+    return "데이터정보보";
   }
 
   @GetMapping("/joinProc")
